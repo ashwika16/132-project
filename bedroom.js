11025c4efa49cb56de21191c68d6195ea065404e
ashwika16 <78404875+ashwika16@users.noreply.github.com>
2022-07-12 19:40:56 +0530

    Add files via upload

---
+++ bedroom.js
@@ -0,0 +1,32 @@
+mg="";
+status1="";
+
+function preload(){
+    img =loadImage('images.jpg');
+}
+function setup(){
+    canvas = createCanvas(640,420);
+    canvas.center();
+    objectDetector =ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML ="Status : Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded");
+    status1 = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error,results){
+    if(error) 
+    {
+        console.log(error);
+        
+    }
+    else{
+        console.log(results);
+        object= results;
+    }
+    
+}
+
